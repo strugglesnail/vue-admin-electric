cdfeb44b4e709c7d5d33e6e87f3d142ebb2dbb4e
strugglesnail <593616590@qq.com>
2021-02-10 08:57:10 +0800

    电力权限管理系统-前端

---
+++ src/api/user.js
@@ -0,0 +1,33 @@
+import request from '@/utils/request'
+import { get, post, postLogin } from '@/utils/config'
+
+export function login(data) {
+  return postLogin('/api/login', data)
+}
+
+export function getInfo(token) {
+  return get('/api/user/info', token)
+}
+
+// export function login(data) {
+//   return request({
+//     url: '/api/login',
+//     method: 'post',
+//     data,
+//     headers: { 'Content-Type': 'application/json' }
+//   })
+// }
+// export function getInfo(token) {
+//   return request({
+//     url: '/vue-admin-template/user/info',
+//     method: 'get',
+//     params: { token }
+//   })
+// }
+
+export function logout() {
+  return request({
+    url: '/vue-admin-template/user/logout',
+    method: 'post'
+  })
+}
---
+++ src/utils/config.js
@@ -0,0 +1,73 @@
+import axios from 'axios'
+import store from '@/store'
+import { getToken } from '@/utils/auth'
+import qs from 'qs'
+// axios.defaults.headers.get['content-type'] = 'application/x-www-form-urlencoded'
+// axios.defaults.headers.post['content-type'] = 'application/json'
+
+const service = axios.create({
+  baseURL: '', // url = base url + request url
+  // withCredentials: true, // send cookies when cross-domain requests
+  timeout: 5000 // request timeout
+})
+
+// request interceptor
+service.interceptors.request.use(
+  config => {
+    // do something before request is sent
+    if (store.getters.token) {
+      config.headers['Authorization'] = getToken()
+    }
+    return config
+  },
+  error => {
+    // do something with request error
+    console.log(error) // for debug
+    return Promise.reject(error)
+  }
+)
+
+export function get(url, params) {
+  return new Promise((resolve, reject) => {
+    service.get(url, {
+      params: params
+    }).then(res => {
+      resolve(res.data)
+    }).catch(err => {
+      reject(err.data)
+    })
+  })
+}
+
+export function post(url, params) {
+  return new Promise((resolve, reject) => {
+    service.post(url, JSON.stringify(params), {
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8'
+      }
+    })
+      .then(res => {
+        resolve(res.data)
+      })
+      .catch(err => {
+        reject(err.data)
+      })
+  })
+}
+
+export function postLogin(url, params) {
+  return service.post(url, JSON.stringify(params), { headers: { 'Content-Type': 'application/json;charset=UTF-8' }
+  })
+}
+
+export function postForm(url, params, indices) {
+  return new Promise((resolve, reject) => {
+    service.post(url, qs.stringify(params, indices))
+      .then(res => {
+        resolve(res.data)
+      })
+      .catch(err => {
+        reject(err.data)
+      })
+  })
+}
